581ca83e8170e3d8022d0cc996a07581cbb543f6
Dominik Baronelli <dbaronelli@shiftphones.com>
2022-10-14 11:56:48 +0200

    (SHIFT) remove ContributorsCloud
    Change-Id: I72a9b052616811f076c769abd354e0d663613139
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- src/org/lineageos/lineageparts/BootReceiver.java
+++ src/org/lineageos/lineageparts/BootReceiver.java
@@ -28,7 +28,7 @@ public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context ctx, Intent intent) {
         // Extract the contributors database
-        ContributorsCloudFragment.extractContributorsCloudDatabase(ctx);
+        /* ContributorsCloudFragment.extractContributorsCloudDatabase(ctx); */
 
         // Toggle visibility of some settings regardless of user type
         LiveDisplaySettings.restoreLiveDisplay(ctx);
